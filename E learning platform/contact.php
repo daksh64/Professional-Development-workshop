6ff7f46b91dcd36293e44bbcd385550bff74f1b2
Aryaman <aryamansemta2025@gmail.com>
2023-09-28 00:11:25 +0530

    Php code added

---
+++ E learning platform/contact.php
@@ -0,0 +1,122 @@
+<?php
+
+include 'components/connect.php';
+
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}else{
+   $user_id = '';
+}
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name']; 
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email']; 
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $number = $_POST['number']; 
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $msg = $_POST['msg']; 
+   $msg = filter_var($msg, FILTER_SANITIZE_STRING);
+
+   $select_contact = $conn->prepare("SELECT * FROM `contact` WHERE name = ? AND email = ? AND number = ? AND message = ?");
+   $select_contact->execute([$name, $email, $number, $msg]);
+
+   if($select_contact->rowCount() > 0){
+      $message[] = 'message sent already!';
+   }else{
+      $insert_message = $conn->prepare("INSERT INTO `contact`(name, email, number, message) VALUES(?,?,?,?)");
+      $insert_message->execute([$name, $email, $number, $msg]);
+      $message[] = 'message sent successfully!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Contact us</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<?php include 'components/user_header.php'; ?>
+
+<!-- contact section starts  -->
+
+<section class="contact">
+
+   <div class="row">
+
+      <div class="image">
+         <img src="images/contact-img.svg" alt="">
+      </div>
+
+      <form action="" method="post">
+         <h3>Contact Us</h3>
+         <input type="text" placeholder="Enter Your Name" required maxlength="100" name="name" class="box">
+         <input type="email" placeholder="Enter Your Mail" required maxlength="100" name="email" class="box">
+         <input type="number" min="0" max="9999999999" placeholder="Phone Number" required maxlength="10" name="number" class="box">
+         <textarea name="msg" class="box" placeholder="Please Enter Your Query" required cols="30" rows="10" maxlength="1000"></textarea>
+         <input type="submit" value="send message" class="inline-btn" name="submit">
+      </form>
+
+   </div>
+
+   <div class="box-container">
+
+      <div class="box">
+         <i class="fas fa-phone"></i>
+         <h3>phone number</h3>
+         <a href="tel:62360-99033">62360-99033</a>
+         <a href="tel:62309-78033">62309-78033</a>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-envelope"></i>
+         <h3>email address</h3>
+         <a href="mailto:aryamansemta2025@gmail.com">aryamansemta2025@gmail.com</a>
+         <a href="mailto:kjasleen117@gmail.com">kjasleen117@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <i class="fas fa-map-marker-alt"></i>
+         <h3>office address</h3>
+         <a href="#">D3 Block 101,Gharuan,Kharar,Punjab,140301</a>
+      </div>
+
+
+   </div>
+
+</section>
+
+<!-- contact section ends -->
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?>  
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+   
+</body>
+</html>
